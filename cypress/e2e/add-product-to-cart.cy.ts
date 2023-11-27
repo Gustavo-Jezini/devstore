22c1536ba09ac896bb01110deadb66aae7f76d16
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-11-27 14:06:38 -0300

    first commit

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,5 @@
+describe('add product to cart', () => {
+  it('should be possible to navigate to the product page and add it to the cart', () => {
+    cy.visit('')
+  })
+})
---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,84 @@
+import { api } from '@/app/data/api'
+import { Product } from '@/app/data/types/products'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hora,
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
+
+  return (
+    <main className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href={`/product/${highlightedProduct.slug}`}
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src={highlightedProduct.image}
+          width={920}
+          height={920}
+          quality={80}
+          alt=""
+          className="group-hover:scale-105 transition-transform duration-500"
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            {highlightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+            })}
+          </span>
+        </div>
+      </Link>
+
+      {otherProducts.map((product) => (
+        <Link
+          key={product.id}
+          href={`/product/${product.slug}`}
+          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src={product.image}
+            width={920}
+            height={920}
+            quality={80}
+            alt=""
+            className="group-hover:scale-105 absolute transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">{product.title}</span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {product.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      ))}
+    </main>
+  )
+}
